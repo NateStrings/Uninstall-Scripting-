31a07714ee8095c52b1de1435139f4a651b43b3e
NateStrings <98417856+NateStrings@users.noreply.github.com>
2022-03-08 07:58:58 -0500

    Update Uninstall Script.ps1

--- Uninstall Script.ps1
+++ Uninstall Script.ps1
@@ -1,2 +1,15 @@
-#Swtich out "Name" for the Application. You can also switch out -Match for -Like and use wildcard * 
+#After a lot more research and talking with other powershell users on the best methods on uninstalling software, I will write out the notes here
+
+#This is the most "Modern" looking approach. 
+#We are avoiding Win32_Product which is a dangerous method because it goes through every program and can mess with your software to the point of damaging it.
+#At this point, its a bit of a dead end.
+
+    #Swtich out "Name" for the Application. You can also switch out -Match for -Like and use wildcard * 
 Get-CimInstance -ClassName Win32_Process | Where-Object name -Match "Name" | Invoke-CimMethod -MethodName "Terminate"
+
+#The other option for us of course is Command Line. You can find most of what you're trying to do simply by typing "Uninstall *app name* Command Line.
+#I'm going to go over a few examples here and try to break them down.
+
+wmic product where name="DNS Agent" call uninstall
+
+
